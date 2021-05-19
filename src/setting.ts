b79de12ee411c6eb16bc786054495afb6114b13b
huka <mr.kharhums@gmail.com>
2021-05-19 17:31:36 +0700

    0.0.5

--- src/setting.ts
+++ src/setting.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 
 export namespace Setting {
-  export const EnableAddingSpaces: boolean = getSetting<boolean>("enableAddingSpaces");
-  export const UpdatedDateFieldName:string = getSetting<string>("updatedDateFieldName");
+  export const EnableAddingSpaces: boolean = getSetting<boolean>("enableAddingSpaces") || true;
+  export const UpdatedDateFieldName: string = getSetting<string>("updatedDateFieldName") || 'lastmod';
 
   function getSetting<T>(field: string): T | undefined {
     return vscode.workspace.getConfiguration("garden-helper").get<T>(field);
